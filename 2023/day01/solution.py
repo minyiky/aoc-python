310ceae8cad1de3df15b999f68247e92a4c83729
minyiky <minyiky@users.noreply.github.com>
2024-11-30 18:32:26 +0000

    Generated files for the new day

---
+++ 2023/day01/solution.py
@@ -0,0 +1,16 @@
+"""Solution for Day 1 - Trebuchet?!"""
+
+def part_one(input_data: str) -> int:
+    """Implement part one logic"""
+    pass
+
+def part_two(input_data: str) -> int:
+    """Implement part two logic"""
+    pass
+
+if __name__ == "__main__":
+    with open("day01/input.txt") as f:
+        input_data = f.read()
+    print("Solution for Day 1 - Trebuchet?!")
+    print(part_one(input_data))  # Run part one
+    print(part_two(input_data))  # Run part two

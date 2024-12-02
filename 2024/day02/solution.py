15358fb9f44cd1c8dc8e2e93e133e9d40d0875b7
minyiky <minyiky@users.noreply.github.com>
2024-12-02 06:50:02 +0000

    Generated files for the new day

---
+++ 2024/day02/solution.py
@@ -0,0 +1,20 @@
+"""Solution for Day 2 - Red-Nosed Reports"""
+
+import os
+
+def part_one(input_data: str) -> int:
+    """Implement part one logic"""
+    pass
+
+def part_two(input_data: str) -> int:
+    """Implement part two logic"""
+    pass
+
+if __name__ == "__main__":
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    with open(f"{dir_path}/input.txt", "r") as f:
+        input_data = f.read()
+    print("Solution for Day {day} - {title}")
+    print(part_one(input_data))  # Run part one
+    print(part_two(input_data))  # Run part two

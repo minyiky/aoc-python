f525441b357e05c5d2ee6c68b13f6370bfa8d4e6
minyiky <mmcdonnell@hotmail.co.uk>
2024-12-10 23:02:53 +0000

    solution for day 10 and day 9 optimisation

--- 2024/day09/solution.py
+++ 2024/day09/solution.py
@@ -2,12 +2,14 @@
 
 import math
 import os
+from collections import defaultdict
+from bisect import insort
 
 
 def part_one(input_data: str) -> int:
     """Implement part one logic"""
     input_data = input_data.strip()
-    files = []
+    files: list[int] = []
     try:
         for i in range(0, len(input_data), 2):
             files += [i // 2] * int(input_data[i])
@@ -30,48 +32,72 @@ def part_one(input_data: str) -> int:
     return total
 
 
+def state(blocks: dict[int, int], empties: dict[int, dict[int, int]]):
+    final_state: list[int] = []
+    for i in range(max(list(blocks.keys())[-1], sorted(list(empties.keys()))[-1]) + 1):
+        if i in blocks:
+            for _ in range(blocks[i]):
+                final_state += [i // 2]
+        elif i in empties:
+            for key, value in empties[i].items():
+                if key == -1:
+                    continue
+                final_state += [key] * value
+            final_state += [-1] * empties[i][-1]
+    return final_state
+
+
 def part_two(input_data: str) -> int:
     """Implement part two logic"""
     input_data = input_data.strip()
     blocks: dict[int, int] = {}
     empties: dict[int, dict[int, int]] = {}
+    gaps: dict[int, list[int]] = defaultdict(list[int])
     try:
         for i in range(0, len(input_data), 2):
             blocks[i] = int(input_data[i])
             empties[i + 1] = {-1: int(input_data[i + 1])}
+            gaps[int(input_data[i + 1])].append(i + 1)
     except IndexError:
         pass
+
+    # print(blocks)
+    # print(empties)
 
     keys = list(blocks.keys())
     to_big_to_fit = math.inf
     for key in keys[::-1]:
+        # print(gaps)
+        # state(blocks, empties)
         if blocks[key] >= to_big_to_fit:
             continue
-        for empty_key, empty_values in empties.items():
-            if empty_key > key:
-                to_big_to_fit = blocks[key]
-                break
-            if empty_values[-1] >= blocks[key]:
-                empties[empty_key][key // 2] = blocks[key]
-                empties[empty_key][-1] -= blocks[key]
-                empties[key] = {-1: blocks[key]}
-                del blocks[key]
-                break
 
-    final_state = []
-    for i in range(max(list(blocks.keys())[-1], sorted(list(empties.keys()))[-1]) + 1):
-        try:
-            if i in blocks:
-                for _ in range(blocks[i]):
-                    final_state += [i // 2]
-            else:
-                for key, value in empties[i].items():
-                    if key == -1:
-                        continue
-                    final_state += [key] * value
-                final_state += [-1] * empties[i][-1]
-        except KeyError:
-            pass
+        possible_gaps: list[int] = []
+        for gap, values in gaps.items():
+            if gap >= blocks[key] and len(values) > 0:
+                possible_gaps.append(values[0])
+
+        if not possible_gaps:
+            to_big_to_fit = blocks[key]
+            continue
+
+        slot = min(possible_gaps)
+
+        if slot > key:
+            to_big_to_fit = blocks[key]
+            continue
+
+        size = empties[slot][-1]
+
+        empties[slot][key // 2] = blocks[key]
+        empties[slot][-1] -= blocks[key]
+        empties[key] = {-1: blocks[key]}
+        del blocks[key]
+
+        insort(gaps[empties[slot][-1]], slot)
+        gaps[size] = gaps[size][1:]
+
+    final_state: list[int] = state(blocks, empties)
     return sum(i * v if v != -1 else 0 for i, v in enumerate(final_state))
 
 
--- 2024/day10/solution.py
+++ 2024/day10/solution.py
@@ -1,16 +1,75 @@
 """Solution for Day 10 - Hoof It"""
 
 import os
+from collections import defaultdict
 
 
 def part_one(input_data: str) -> int:
     """Implement part one logic"""
-    pass
+    lines = input_data.splitlines()
+    loc_map: dict[tuple[int, int], set[tuple[int, int]]] = defaultdict(
+        set[tuple[int, int]], set({})
+    )
+    grid: dict[tuple[int, int], int] = defaultdict(int)
+
+    for j, line in enumerate(lines):
+        for i, char in enumerate(line):
+            try:
+                grid[(i, j)] = int(char)
+            except ValueError:
+                pass
+            if char == "0":
+                loc_map[(i, j)].add((i, j))
+
+    directions = ([1, 0], [-1, 0], [0, 1], [0, -1])
+
+    for i in range(1, 10):
+        new_map: dict[tuple[int, int], set[tuple[int, int]]] = defaultdict(
+            set[tuple[int, int]], set({})
+        )
+
+        for point, values in loc_map.items():
+
+            for direction in directions:
+                new_point = (point[0] + direction[0], point[1] + direction[1])
+                if grid[new_point] == i:
+                    new_map[new_point] = new_map[new_point].union(values)
+
+        loc_map = new_map
+
+    return sum(len(v) for v in loc_map.values())
 
 
 def part_two(input_data: str) -> int:
     """Implement part two logic"""
-    pass
+    lines = input_data.splitlines()
+    loc_map: dict[tuple[int, int], int] = defaultdict(int)
+    grid: dict[tuple[int, int], int] = defaultdict(int)
+
+    for j, line in enumerate(lines):
+        for i, char in enumerate(line):
+            try:
+                grid[(i, j)] = int(char)
+            except ValueError:
+                pass
+            if char == "0":
+                loc_map[(i, j)] += 1
+
+    directions = ([1, 0], [-1, 0], [0, 1], [0, -1])
+
+    for i in range(1, 10):
+        new_map: dict[tuple[int, int], int] = defaultdict(int)
+
+        for point, values in loc_map.items():
+
+            for direction in directions:
+                new_point = (point[0] + direction[0], point[1] + direction[1])
+                if grid[new_point] == i:
+                    new_map[new_point] += values
+
+        loc_map = new_map
+
+    return sum(loc_map.values())
 
 
 if __name__ == "__main__":
